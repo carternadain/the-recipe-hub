9a803f128997e6e31dbd0668bf6d21def567e88a
mmilner29 <madisonm4514@gmail.com>
2022-03-13 10:20:25 -0700

    center align

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+var foodSubmit = document.querySelector("#food-generate")
+var cocktailSubmit = document.querySelector("#cocktail-generate")
+
 var fetchFunction = function() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 };
 fetchFunction();
+
+var getFoodRecipe = function(event) {
+    event.preventDefault();
+    console.log("generate food");
+};
+
+var getCocktailRecipe = function(event) {
+    event.preventDefault();
+    console.log("generate cocktail");
+};
+
+foodSubmit.addEventListener("submit", getFoodRecipe);
+cocktailSubmit.addEventListener("submit", getCocktailRecipe)
