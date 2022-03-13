0e86ce79e40d5c945f6246b3dd38737c4c5bebe4
mmilner29 <madisonm4514@gmail.com>
2022-03-13 11:37:47 -0700

    event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,41 @@
 // Declare constants and variables
 
-
-
-
+var foodSubmit = document.querySelector("#food-generate")
+var cocktailSubmit = document.querySelector("#cocktail-generate")
 
 
 
 // retrieve local storage
 
 
-
-
 // function to execute when the generate button is clicked
 
 
-// function to render cocktail recipe
+// fection API
+var fetchFunction = function() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+};
+fetchFunction();
 
+// function to render cocktail recipe
+var getCocktailRecipe = function(event) {
+    event.preventDefault();
+    console.log("generate cocktail");
+};
 
 // function to render the food recipe 
 
+var getFoodRecipe = function(event) {
+    event.preventDefault();
+    console.log("generate food");
+};
 
 // function to populate search history below search bar
 
 
 // generate button click functionality
+foodSubmit.addEventListener("submit", getFoodRecipe);
+cocktailSubmit.addEventListener("submit", getCocktailRecipe)
+
 
 
