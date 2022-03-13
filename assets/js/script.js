adebc3bd9083cb846f65293e6f11930e18224171
mmilner29 <madisonm4514@gmail.com>
2022-03-13 14:41:18 -0700

    add function to appead item to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // Declare constants and variables
 
-var foodSubmit = document.querySelector("#food-generate")
-var cocktailSubmit = document.querySelector("#cocktail-generate")
+const foodSubmit = document.querySelector("#food-generate");
+const cocktailSubmit = document.querySelector("#cocktail-generate");
 
+var foodEl = document.querySelector("#food-element");
 
 
 // retrieve local storage
@@ -12,30 +13,45 @@ var cocktailSubmit = document.querySelector("#cocktail-generate")
 
 
 // fection API
-var fetchFunction = function() {
+var fetchCocktailFunction = function() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then(function(drinks) {
+        return drinks.json();
+      })
+    .then(function(drinks) {
+    drinks.json().then(function(data) {
+        console.log(data);
+    });
+});
 };
-fetchFunction();
+
+var fetchFoodFunction = function() {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then(response => response.json())
+    .then(response => {
+        getFoodRecipe(response.meals[0])
+    });
+};
+
 
 // function to render cocktail recipe
-var getCocktailRecipe = function(event) {
-    event.preventDefault();
-    console.log("generate cocktail");
+var getCocktailRecipe = function() {
+
 };
 
 // function to render the food recipe 
 
-var getFoodRecipe = function(event) {
-    event.preventDefault();
-    console.log("generate food");
+function getFoodRecipe(meal) {
+    foodEl.innerHTML = 
+    '<h2>${meal.strMeal}</h2>'
+
 };
 
 // function to populate search history below search bar
 
-
 // generate button click functionality
-foodSubmit.addEventListener("submit", getFoodRecipe);
-cocktailSubmit.addEventListener("submit", getCocktailRecipe)
+foodSubmit.addEventListener("click", fetchFoodFunction);
+cocktailSubmit.addEventListener("click", getCocktailRecipe);
 
 
 
